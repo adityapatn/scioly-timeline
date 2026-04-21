320c29839b9394a8ddc1a6c0c2f79cdff177b312
Aditya Patnaik <aditya.patnaik.email@gmail.com>
2026-04-21 09:45:57 -0700

    Delete duosmium-main directory

--- duosmium-main/assets/index.js
+++
@@ -1,7 +0,0 @@
-import "./imports.scss";
-import "./results_index/";
-import "./results_template/";
-import "./results_placeholder/";
-import "./results_schools/";
-import "./results_upcoming/";
-import "instant.page";
